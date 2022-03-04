c3204781059b5a5af3963422a2fa818442b0ca70
shun0918 <kkabosu386@gmail.com>
2022-03-04 17:56:18 +0900

    add: React追加

---
+++ app/javascript/components/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => (
+    <div className="app">this is JSX</div>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
