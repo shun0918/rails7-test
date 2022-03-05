44c96a750dd34614783d8395652b5d096ac5b29a
shun0918 <kkabosu386@gmail.com>
2022-03-05 10:17:22 +0900

    feature: mui追加

--- app/javascript/components/index.tsx
+++ app/javascript/components/index.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Button from '@mui/material/Button';
 
 const App = () => (
-    <div className="app">Hello Rails!</div>
+    <div>
+        <div className="app">Hello Rails!</div>
+        <Button variant="contained" >clickme</Button>
+    </div>
 );
 
 document.addEventListener('DOMContentLoaded', () => {
