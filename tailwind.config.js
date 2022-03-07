3a122e1c1ff90f298fa6e0ef0315aae942556130
shun0918 <kkabosu386@gmail.com>
2022-03-07 17:01:25 +0900

    add: tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/assets/stylesheets/**/*.css',
+    './app/javascript/**/*.{js,jsx,ts,tsx}',
+    './app/javascript/components/**/*.{js,jsx,ts,tsx}',
+  ],
+  important: '#root',
+  // theme: {
+  //   extend: {},
+  // },
+  // plugins: [],
+}
