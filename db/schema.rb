3718c6642b3649f2c5c31149c65f3116a8ab55c8
shun0918 <kkabosu386@gmail.com>
2022-03-10 08:50:40 +0900

    feat: dbのパスワードをdigest化

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_06_083100) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_09_234504) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
