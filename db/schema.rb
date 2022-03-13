a8d3c870c32496eef3966510e47a5d658f746691
shun0918 <kkabosu386@gmail.com>
2022-03-13 17:15:11 +0900

    add: session保持

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_09_234504) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_10_100724) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_token"
   end
 
 end
