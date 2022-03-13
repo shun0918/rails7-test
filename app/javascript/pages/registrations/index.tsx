69713b09858b163fbe7f85b92bf2b1a1e42568e7
shun0918 <kkabosu386@gmail.com>
2022-03-13 19:02:23 +0900

    fix: 同ページでの遷移でDOMのレンダリングがされない

--- app/javascript/pages/registrations/index.tsx
+++ app/javascript/pages/registrations/index.tsx
@@ -2,7 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import RegistrationMain from "../../components/registrations/RegistrationMain";
 
-document.addEventListener('DOMContentLoaded', () => {
+const main = () => {
     const rootEl = document.getElementById('root')
     ReactDOM.render(<RegistrationMain />, rootEl)
-})
+}
+
+if (document.readyState !== "loading") {
+    main();
+} else {
+    document.addEventListener("DOMContentLoaded", main, false);
+}
