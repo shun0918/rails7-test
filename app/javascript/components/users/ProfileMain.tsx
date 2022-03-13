f730662ea6aadaaee4d2f173302c0d2672593ada
shun0918 <kkabosu386@gmail.com>
2022-03-13 19:17:57 +0900

    docs: Userモデル更新

--- app/javascript/components/users/ProfileMain.tsx
+++ app/javascript/components/users/ProfileMain.tsx
@@ -1,6 +1,23 @@
 import React, { useEffect, useState } from "react";
+import { apiClient } from "../../libs/api/client";
+import { User } from '../../types/models/User';
 
+type Res = {
+    users: User
+}
 const ProfileMain = () => {
+    const [user, setUser] = useState({});
+    const profile = async () => {
+        const res = await apiClient.get<Res>('/')
+        if(!res.data) {
+            console.error(res.errors);
+            return
+        }
+        setUser(res.data);
+    }
+    useEffect(() => {
+        profile;
+    },[])
     return (
         <div>Profile</div>
     )
