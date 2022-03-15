dfb336345c8f975fae4bf38aba0eb6e196d94d9d
shun0918 <kkabosu386@gmail.com>
2022-03-15 20:43:53 +0900

    feat: add api  -> fetch profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
   def index
   end
+
+  def me
+    @profile = Profile.find_by(user_id: @current_user.id)
+    render json: { user: @current_user, profile: @profile}
+  end
 end
