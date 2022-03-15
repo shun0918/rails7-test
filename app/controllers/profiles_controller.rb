b5b32248581705023a4a5b8dc695540b52c28b40
shun0918 <kkabosu386@gmail.com>
2022-03-15 20:45:02 +0900

    feat: add api -> update profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,25 @@
 class ProfilesController < ApplicationController
+  def create
+    if params[:image].present?
+      @image = Image.new(path: params[:image])
+    end
+
+    # 作成
+    if Profile.exists?(user_id: @current_user.id)
+      @profile = Profile.update(profile_params(image_id))
+    else
+      @profile = Profile.new(profile_params(image_id))
+    end
+
+    render json: create_response({success: true, profile: @profile})
+  end
+
+  private
+    def image_id
+      @image.presence? @image.id : nil
+
+
+    def profile_params(image_id: nil)
+      params.require(:profile).permit(:name, :bio, :phone, :image).marge(user_id: @current_user.id, image_id:)
+    end
 end
