fbacf5d5f3abcf36bedf89fe2953f97bc0638421
shun0918 <kkabosu386@gmail.com>
2022-03-16 19:07:54 +0900

    chore: allow ip

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,5 +68,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.web_console.whitelisted_ips = '172.24.0.1' # PCのIP
+  config.web_console.whitelisted_ips =  '0.0.0.0/0' # PCのIP
 end
