705531c4bbe99d594ab7a3d7c9bf8f390f430bbb
shun0918 <kkabosu386@gmail.com>
2022-03-16 18:59:21 +0900

    feat: new component: UploadButton

---
+++ app/javascript/components/ui/buttons/UploadButton.tsx
@@ -0,0 +1,18 @@
+import { Button, Input } from "@mui/material";
+import React from "react";
+import { OnChangeEventSetter } from "../../../types/utils/react";
+
+type Props = {
+  onChange: OnChangeEventSetter;
+}
+const UploadButton = ({onChange}: Props) => {
+  return (
+    <label htmlFor="contained-button-file">
+      <input onChange={onChange} accept="image/*" id="contained-button-file" multiple type="file" className="hidden" />
+      <Button variant="contained" component="span">
+        Upload
+      </Button>
+    </label>
+  )
+}
+export default UploadButton;
