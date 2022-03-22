6b0eec69e8a005f0b1640c7c2981e2710ab9b07d
shun0918 <kkabosu386@gmail.com>
2022-03-22 17:18:59 +0900

    feat: adjust ui

--- app/javascript/components/tasks/TaskBoard.tsx
+++ app/javascript/components/tasks/TaskBoard.tsx
@@ -4,10 +4,10 @@ import TaskCard from './TaskCard';
 
 const TaskBoard = () => {
   return (
-    <div className="w-screen p-8 rounded-xl bg-blue-50">
-      <div className="flex overflow-x-auto">
+    <div className="w-screen">
+      <div className="mx-8 p-8 rounded-xl bg-blue-50 flex overflow-x-auto">
         {[...Array(7)].map((_, i) => (
-          <div className="w-96 flex-shrink-0 px-4" key={i}>
+          <div className="w-64 flex-shrink-0 px-4" key={i}>
             <Typography marginBottom={2}>Task{i}</Typography>
             <div className="grid gap-y-8">
               <TaskCard />
