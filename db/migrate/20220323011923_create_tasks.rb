13bd796a630ed2621af0f21dda99fb40a232d68b
shun0918 <kkabosu386@gmail.com>
2022-03-23 12:31:33 +0900

    fix: db:rollback for rename

--- db/migrate/20220323011923_create_tasks.rb
+++ db/migrate/20220323011923_create_tasks.rb
@@ -2,8 +2,8 @@ class CreateTasks < ActiveRecord::Migration[7.0]
   def change
     create_table :tasks do |t|
       t.string :title
-      t.references :taxonomies, null: false, foreign_key: true
-      t.references :user_files, null: false, foreign_key: true
+      t.references :taxonomy, null: false, foreign_key: true
+      t.references :user_file, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -53,12 +53,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_23_011923) do
 
   create_table "tasks", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
-    t.bigint "taxonomies_id", null: false
-    t.bigint "user_files_id", null: false
+    t.bigint "taxonomy_id", null: false
+    t.bigint "user_file_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["taxonomies_id"], name: "index_tasks_on_taxonomies_id"
-    t.index ["user_files_id"], name: "index_tasks_on_user_files_id"
+    t.index ["taxonomy_id"], name: "index_tasks_on_taxonomy_id"
+    t.index ["user_file_id"], name: "index_tasks_on_user_file_id"
   end
 
   create_table "taxonomies", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -86,7 +86,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_23_011923) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "profiles", "users"
-  add_foreign_key "tasks", "taxonomies", column: "taxonomies_id"
-  add_foreign_key "tasks", "user_files", column: "user_files_id"
+  add_foreign_key "tasks", "taxonomies"
+  add_foreign_key "tasks", "user_files"
   add_foreign_key "user_files", "users"
 end
