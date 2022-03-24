f7548368ce83b3bb4b99a4cbfe3900f6277484c5
shun0918 <kkabosu386@gmail.com>
2022-03-24 16:42:08 +0900

    feature: draggable tasks Not yet connected to the API)

--- app/javascript/components/tasks/TaskBoard.tsx
+++ app/javascript/components/tasks/TaskBoard.tsx
@@ -3,6 +3,13 @@ import React, { useEffect, useState } from 'react';
 import { Status } from '../../types/models/Status';
 import { Task, EditableTask } from '../../types/models/Task';
 import TaskCard from './TaskCard';
+import {
+  DragDropContext,
+  Droppable,
+  Draggable,
+  DropResult,
+  ResponderProvided,
+} from 'react-beautiful-dnd';
 
 type Props = {
   tasks: Task[];
@@ -48,6 +55,37 @@ const TaskBoard: React.FC<Props> = ({ tasks, status, onCreateTask }) => {
       [task.status_id]: [...taskMap[task.status_id].slice(0, -1)],
     });
   };
+  const onDragEnd = (result: DropResult, provided: ResponderProvided) => {
+    // const items = Array.from()
+    console.log(result);
+    console.log(provided);
+    if (
+      !result.destination ||
+      (result.destination.droppableId === result.source.droppableId &&
+        result.destination.index === result.source.index)
+    )
+      return;
+    const sourceItems = taskMap[Number(result.source.droppableId)];
+    console.log('sourceItems', sourceItems);
+    const [reorderedItem] = sourceItems.splice(result.source.index, 1);
+    const destinationItems =
+      result.source.droppableId === result.destination.droppableId
+        ? sourceItems
+        : taskMap[Number(result.destination.droppableId)];
+    destinationItems.splice(result.destination.index, 0, reorderedItem);
+
+    let newTaskMap = {
+      ...taskMap,
+      [result.destination.droppableId]: destinationItems,
+    };
+    if (result.source.droppableId !== result.destination.droppableId) {
+      newTaskMap = {
+        ...newTaskMap,
+        [result.source.droppableId]: sourceItems,
+      };
+    }
+    setTaskMap(newTaskMap);
+  };
   useEffect(() => {
     const getTaskMap = (): Record<number, Task[]> => {
       if (!status.length || !tasks.length) return {};
@@ -73,27 +111,48 @@ const TaskBoard: React.FC<Props> = ({ tasks, status, onCreateTask }) => {
   return (
     <div className="w-screen">
       <div className="mx-8 p-8 rounded-xl bg-blue-50 flex overflow-x-auto">
-        {status.map((s) => (
-          // col
-          <div className="w-64 flex-shrink-0 px-4" key={s.id}>
-            <Typography marginBottom={2}>{s.name}</Typography>
-            <div className="grid gap-y-8">
-              {taskMap[s.id]
-                ? taskMap[s.id].map((_task) => (
-                    <TaskCard
-                      key={`${s.id}_${_task.id}`}
-                      task={_task}
-                      onSaveTask={onSaveTask}
-                      onCancel={() => onCancelCreate(_task)}
-                    />
-                  ))
-                : null}
+        <DragDropContext onDragEnd={onDragEnd}>
+          {status.map((s) => (
+            // col
+            <div className="w-64 flex-shrink-0 px-4" key={s.id}>
+              <Typography marginBottom={2}>{s.name}</Typography>
+              <Droppable droppableId={String(s.id)}>
+                {(provided) => (
+                  <ul {...provided.droppableProps} ref={provided.innerRef}>
+                    {taskMap[s.id]
+                      ? taskMap[s.id].map((_task, index) => (
+                          <Draggable
+                            key={`${s.id}_${_task.id}`}
+                            draggableId={`${_task.id}`}
+                            index={index}
+                          >
+                            {(provided) => (
+                              <li
+                                ref={provided.innerRef}
+                                {...provided.draggableProps}
+                                {...provided.dragHandleProps}
+                                className="mt-8"
+                              >
+                                <TaskCard
+                                  task={_task}
+                                  onSaveTask={onSaveTask}
+                                  onCancel={() => onCancelCreate(_task)}
+                                />
+                              </li>
+                            )}
+                          </Draggable>
+                        ))
+                      : null}
+                    {provided.placeholder}
+                  </ul>
+                )}
+              </Droppable>
               <Button onClick={() => addTask(s.id)} variant="outlined" fullWidth>
                 Add new task +
               </Button>
             </div>
-          </div>
-        ))}
+          ))}
+        </DragDropContext>
       </div>
     </div>
   );
