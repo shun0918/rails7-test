8a2a0fa1c72e827c4fa6dbba4bc6522b72404241
shun0918 <kkabosu386@gmail.com>
2022-03-26 17:52:36 +0900

    feat: hidden when card is not hoverd

--- app/javascript/components/tasks/TaskCard.tsx
+++ app/javascript/components/tasks/TaskCard.tsx
@@ -99,12 +99,14 @@ const TaskCard: React.FC<Props> = ({ task, editable = false, onSaveTask, onCance
       </div>
       {
         editable ? null :
-        <div className="absolute top-2 right-2">
-          <div className="relative" ref={menuElm} id={taskCardId}>
-            <IconButton onClick={onClickMenu} aria-label="Example">
-              <MoreHorizIcon />
-            </IconButton>
-            {isOpenMenu ? <TaskCardInnerMenu onDelete={onDelete} /> : null}
+        <div className="absolute top-0 left-0 w-full h-full opacity-0 hover:opacity-100 transition-opacity">
+          <div className="absolute top-2 right-2">
+            <div className="relative" ref={menuElm} id={taskCardId}>
+              <IconButton onClick={onClickMenu} aria-label="Example">
+                <MoreHorizIcon />
+              </IconButton>
+              {isOpenMenu ? <TaskCardInnerMenu onDelete={onDelete} /> : null}
+            </div>
           </div>
         </div>
       }
