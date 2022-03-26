8b65d0e4e4458d03d332852e0f96c13bf3bd1df5
shun0918 <kkabosu386@gmail.com>
2022-03-26 17:03:19 +0900

    fix: enable routing

--- app/javascript/pages/tasks/index.tsx
+++ app/javascript/pages/tasks/index.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TaskMain from '../../components/tasks/TaskMain';
 
-document.addEventListener('DOMContentLoaded', () => {
+const main = () => {
   const rootEl = document.getElementById('root');
   ReactDOM.render(<TaskMain />, rootEl);
-});
+};
+
+if (document.readyState !== 'loading') {
+  main();
+} else {
+  document.addEventListener('DOMContentLoaded', main, false);
+}
