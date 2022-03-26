f574bb52447c328dbeb0d9f9ecb51ab27268ebd6
shun0918 <kkabosu386@gmail.com>
2022-03-26 17:30:48 +0900

    feat: update task with pos

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,19 +3,30 @@ class TasksController < ApplicationController
   end
 
   def show
-    @tasks = @current_user.tasks.all
+    @tasks = @current_user.tasks.all.order(:pos)
     @status = Status.all
     render json: { tasks: @tasks, status: @status }
   end
 
   def create
-    @task = Task.create(task_params)
+    max_pos = Task.where(user_id: @current_user.id).maximum(:pos) || 0
+    @task = Task.create(task_params pos: max_pos += 1)
     render json: { task: @task }
   end
 
   def update
     @task = Task.find(params[:task][:id])
-    @task.update(task_params)
+    if params[:index] == 0
+      pos = (Task.where(user_id: @current_user.id, status_id: params[:task][:status_id]).minimum(:pos) || 1) / 2
+    else
+      @tasks = Task.where(user_id: @current_user.id, status_id: params[:task][:status_id])
+          .where.not(id: @task.id)
+          .order(:pos)
+          .limit(2)
+          .offset(params[:index] - 1)
+      pos = @tasks.size < 2 ? @tasks.last[:pos] + 1 : (@tasks[0][:pos] + @tasks[1][:pos]) / 2
+    end
+    @task.update(task_params(pos: pos))
   end
 
   def delete
@@ -29,7 +40,7 @@ class TasksController < ApplicationController
   end
 
   private
-    def task_params
-      params.require(:task).permit(:title, :status_id).merge(user_id: @current_user.id)
+    def task_params(pos:)
+      params.require(:task).permit(:title, :status_id).merge(user_id: @current_user.id, pos: pos)
     end
 end
