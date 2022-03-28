2200a7fe3fd17e8ef6b25bc245465385eb54a505
shun0918 <kkabosu386@gmail.com>
2022-03-28 22:49:49 +0900

    refactor: remove unused file

--- app/javascript/components/hooks/useProfile.ts
+++
@@ -1,8 +0,0 @@
-import { useState } from 'react';
-
-const useCopy = <T>(originValue: T) => {
-  const [copiedValue, setCopiedValue] = useState<T>(originValue);
-  return [copiedValue, setCopiedValue];
-};
-
-export default useCopy;
