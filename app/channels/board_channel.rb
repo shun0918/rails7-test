3ebd08c0de71fb234d2d43b798f0f8db236f2ae6
shun0918 <kkabosu386@gmail.com>
2022-03-29 18:29:07 +0900

    refactor: remove unused method

--- app/channels/board_channel.rb
+++ app/channels/board_channel.rb
@@ -4,10 +4,6 @@ class BoardChannel < ApplicationCable::Channel
     stream_from "task_board_#{current_user.id.to_s}"
   end
 
-  def receive(data)
-    ActionCable.server.broadcast("task_board_#{current_user.id.to_s}", data);
-  end
-
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
